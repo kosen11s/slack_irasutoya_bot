da2710b922e5f704c07570fcb36026ffe36907ca
long-long-float <niinikazuki@yahoo.co.jp>
2016-05-13 14:02:48 +0900

    fetch sections and labels

--- scraper/fetch.rb
+++ scraper/fetch.rb
@@ -1,28 +1,35 @@
 require 'mechanize'
 require 'json'
+require 'ruby-progressbar'
 
-agent = Mechanize.new
+@agent = Mechanize.new
 
-page = agent.get("http://www.irasutoya.com/search/label/コンピューター")
-
-current_page = page
-image_urls = []
-1.times do
-  image_urls.concat(current_page.search('div.boxim>a').map{|box| box['href'] })
-
-  next_link = page.at('a.blog-pager-older-link')&.send(:[], 'href')
-  break unless next_link
-
-  current_page = agent.get(next_link)
+def fetch_labels(section_url)
+  page = @agent.get(section_url)
+  page.search('#banners>a').map{|a| a['href'] }
 end
 
-images = image_urls.take(5).map do |url|
-  page = agent.get(url)
+def fetch_images(label_url)
+  current_page = @agent.get(label_url)
+  image_urls = []
+  1.times do
+    image_urls.concat(current_page.search('div.boxim>a').map{|box| box['href'] })
+
+    next_link = current_page.at('a.blog-pager-older-link')&.send(:[], 'href')
+    break unless next_link
+
+    current_page = @agent.get(next_link)
+  end
+  image_urls
+end
+
+def fetch_image_detail(url)
+  page = @agent.get(url)
 
   title = page.at('div.title>h2').text.strip
   entry = page.search('div.entry>div.separator')
   image_url = entry[0].at('a')['href']
-  description = entry[1].text
+  description = entry[1].text.strip
   categories = page.search('span.category>a').map(&:text)
 
   {
@@ -32,5 +39,19 @@ images = image_urls.take(5).map do |url|
     categories: categories
   }
 end
+
+page = @agent.get('http://www.irasutoya.com/')
+
+sections = page.search('#section_banner>a').map{|a| a['href'] }
+labels = sections.take(2).map{|section| fetch_labels(section).take(2) }.flatten
+image_urls = labels.map{|label| fetch_images(label).take(2) }.flatten
+
+progressbar = ProgressBar.create(total: image_urls.length, output: STDERR)
+images = image_urls.uniq.map do |image_url|
+  i = fetch_image_detail(image_url)
+  progressbar.increment
+  i
+end
+progressbar.finish
 
 puts images.to_json
